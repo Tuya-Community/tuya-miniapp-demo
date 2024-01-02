ee2b830aab6b80c285d63b93182d8c0f73617fd0
HandsomeFishYin <346107057@qq.com>
2024-01-02 14:05:18 +0800

    fix: 安卓权限问题

--- getImageData/pages/cropImage/index.js
+++ getImageData/pages/cropImage/index.js
@@ -17,10 +17,23 @@ Page({
   },
   getData() {
     let self = this
-    ty.chooseCropImage({
-      success(res) {
-        const p = res.path
-        self.render.draw(p)
+    // 安卓兼容性问题，需要先请求获取相册权限
+    ty.authorize({
+      scope: 'scope.writePhotosAlbum',
+      success(e) {
+        console.log('authorize success', e)
+        ty.chooseCropImage({
+          success(res) {
+            const p = res.path
+            self.render.draw(p)
+          },
+        })
+      },
+      fail(e) {
+        console.log('authorize fail', e)
+      },
+      complete(e) {
+        console.log('authorize complete', e)
       },
     })
   },
