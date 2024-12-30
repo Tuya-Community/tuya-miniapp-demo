3811536ab20a3bce793560fad3bd5a586c024535
Ulan Xuan <mifind@sina.com>
2024-12-30 13:54:36 +0800

    feat: 增加 CDN Demo（Ray、原生） (#21)
    * feat: 增加 RayCdn
    
    * feat: 增加原生小程序 CDN Demo
    
    ---------
    
    Co-authored-by: 牧知 <muzhi.xuan@tuya.com>

---
+++ rayCdn/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+}
