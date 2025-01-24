f52d1b07f886d4ca9b83bef6299d0105f7f4d6f7
牧知 <muzhi.xuan@tuya.com>
2025-01-24 15:19:56 +0800

    feat: update 原生 cdn demo

---
+++ cdn/app.js
@@ -0,0 +1,14 @@
+App({
+  // onLaunch() {
+  //   console.log('onLaunch')
+  // },
+  // onShow() {
+  //   console.log('onLaunch')
+  // },
+  // onHide() {
+  //   console.log('onHide')
+  // },
+  // onError() {
+  //   console.log('onError')
+  // },
+})
